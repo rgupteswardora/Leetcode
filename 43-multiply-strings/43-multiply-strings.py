c8d6c6fb86185b9b5254639ee81e130b7326c494
rgupteswardora <96424349+rgupteswardora@users.noreply.github.com>
2022-02-12 02:28:44 +0530

    Time: 16 ms (96.88%), Space: 13.5 MB (81.84%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        num1=int(num1)
+        num2=int(num2)
+        return str(num1*num2)
