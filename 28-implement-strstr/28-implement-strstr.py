8ef1e51567ed05a8e5ebf963c0782922c248f273
rgupteswardora <96424349+rgupteswardora@users.noreply.github.com>
2022-02-20 21:35:22 +0530

    Time: 696 ms (26.49%), Space: 14.7 MB (30.83%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        rt=-1
+        count=0
+        if(haystack==needle):
+            return 0
+        elif(needle==""):
+            return 0
+        elif(len(haystack)<len(needle)):
+            return -1
+        for i in range(len(haystack)+1-len(needle)):
+            if(haystack[i:i+len(needle)]==needle):
+                return i
+        return -1
