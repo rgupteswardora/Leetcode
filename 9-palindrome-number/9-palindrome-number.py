3ed3e0db206433f6fa6d268b094ca2a3bc429dff
rgupteswardora <96424349+rgupteswardora@users.noreply.github.com>
2022-02-20 20:22:53 +0530

    Time: 36 ms (98.83%), Space: 13.4 MB (66.59%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        st=str(x)
+        if st==st[::-1]:
+            return True
+        return False
+        
