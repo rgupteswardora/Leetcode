0a93fee8972249e8e0854b667e0753ecda1302ee
R gupteswar Dora <96424349+rgupteswardora@users.noreply.github.com>
2022-02-25 20:04:09 +0530

    Time: 68 ms (69.99%), Space: 13.5 MB (14.40%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -4,8 +4,12 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
+        #121 121
+        #345 543
         st=str(x)
         if st==st[::-1]:
             return True
         return False
+        
+        
         
