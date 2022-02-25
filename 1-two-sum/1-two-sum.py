c6465ca598f1f9e72f7cfb1ff077b377e7c9ca68
R gupteswar Dora <96424349+rgupteswardora@users.noreply.github.com>
2022-02-25 19:58:36 +0530

    Time: 7077 ms (7.54%), Space: 14.5 MB (10.18%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -6,8 +6,7 @@ class Solution(object):
         :rtype: List[int]
         """
         for i in range(0,len(nums)):
-            for j in range(i+1,len(nums)):
-                if(nums[i]+nums[j]==target):
-                    return [i,j]
-            
-        
+            for j in range(0,len(nums)):
+                if i!=j:
+                    if nums[i]+nums[j]==target:
+                        return [i,j]
