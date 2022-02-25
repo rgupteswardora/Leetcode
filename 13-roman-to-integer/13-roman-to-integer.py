6a9c08d0f1d70794245988af1c66ca6b14c9e694
R gupteswar Dora <96424349+rgupteswardora@users.noreply.github.com>
2022-02-25 20:20:59 +0530

    Time: 36 ms (90.70%), Space: 13.5 MB (32.43%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -6,12 +6,15 @@ class Solution(object):
         """
         val={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
         num=0
-        le=list(s)
-        while(len(le)!=0):
-            st=le.pop(0)
-            if(len(le)!=0 and val[st]<val[le[0]]):
-                num=num+(val[le[0]]-val[st])
-                le.pop(0)
+        i=0
+        lastindex=len(s)-1
+        while(i<lastindex):
+            if(val[s[i+1]]>val[s[i]]):
+                num=num+val[s[i+1]]-val[s[i]]
+                i+=2
             else:
-                num=num+val[st]
+                num=num+val[s[i]]
+                i+=1
+        if i==lastindex:
+            num=num+val[s[i]]
         return num
